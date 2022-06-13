3f9ef6bce297c72eaa406c705709fd06cb60c022
RogerCastillo877 <roger.castillo877@gmail.com>
2022-06-13 17:32:44 -0500

    Problem with unique responsability

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -9,7 +9,50 @@
       public birthdate: Date ) {}
   }
 
-  const newPerson = new Person( 'Alex', 'M', new Date( '1985-10-21' ) );
-  console.log({ newPerson });
+  class User extends Person {
+
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ) {
+      super ( name, gender, birthdate );
+      this.lastAccess = new Date();
+    }
+
+    checkCredentials() {
+      return true;
+    }
+  }
+
+  class UserSettings extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      email: string,
+      role: string,
+      name: string,
+      gender: Gender,
+      birthdate: Date
+    ){
+      super( name, gender, birthdate , email, role )
+    }  
+  }
+
+  const userSettings = new UserSettings(
+    '/usr/home',
+    '/home',
+    'email@email.com',
+    'Admin',
+    'Rocas',
+    'M',
+    new Date('1985-10-21')
+  );
+
+  console.log({ userSettings });
   
 })();
