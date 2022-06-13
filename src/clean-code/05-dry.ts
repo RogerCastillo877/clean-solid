0feddf759a78abbdf709b660e89b92abf8f71348
RogerCastillo877 <roger.castillo877@gmail.com>
2022-06-13 16:27:26 -0500

    refactor DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,10 +7,28 @@ class Product {
     public size:Size = ''
   ) {}
 
+  isProductReady(): boolean {
+    for( const key in this ) {
+      switch ( typeof this[key] ) {
+        case 'string':
+          if( (<string><unknown>this[key]).length <= 0 ) throw new Error( `${ key } is empry` )
+          break;
+        case 'number':
+          if( (<number><unknown>this[key]) <= 0 ) throw new Error( `${ key } is zero` )
+          break;
+        default:
+          throw new Error( `${ typeof key } is not supported` );
+      }
+    }
+    return true;
+  };
+
   toString() {
-    if( this.name.length <= 0 ) throw new Error('Name is empty');
-    if( this.price <= 0 ) throw new Error('Price is zero');
-    if( this.size.length <= 0 ) throw new Error('Size is empty');
+    // if( this.name.length <= 0 ) throw new Error('Name is empty');
+    // if( this.price <= 0 ) throw new Error('Price is zero');
+    // if( this.size.length <= 0 ) throw new Error('Size is empty');
+
+    if( !this.isProductReady ) return;
 
     return `${this.name} (${this.price}), ${ this.size}`
   }
