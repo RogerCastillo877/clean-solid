9e0ec9dad81b18efff31fb141fc207583a4c6c88
RogerCastillo877 <roger.castillo877@gmail.com>
2022-06-14 15:12:30 -0500

    Application of Single Responsibility Principle

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -5,36 +5,64 @@
       name: string;
   }
   
-  // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-  // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
-  class ProductBloc {
-  
-      loadProduct( id: number ) {
-          // Realiza un proceso para obtener el producto y retornarlo
-          console.log('Producto: ',{ id, name: 'OLED Tv' });
-      }
-  
-      saveProduct( product: Product ) {
-          // Realiza una petición para salvar en base de datos 
-          console.log('Guardando en base de datos', product );
-      }
-  
-      notifyClients() {
-          console.log('Enviando correo a los clientes');
-      }
-  
-      onAddToCart( productId: number ) {
-          // Agregar al carrito de compras
-          console.log('Agregando al carrito ', productId );
-      }
-  
-  }
-  
-  const productBloc = new ProductBloc();
+  class ProductService {
 
-  productBloc.loadProduct(10);
-  productBloc.saveProduct({ id: 10, name: 'OLED TV' });
-  productBloc.notifyClients();
-  productBloc.onAddToCart(10);
+    
+    getProduct( id: number ) {
+      console.log('Producto: ',{ id, name: 'OLED Tv' });
+    };
+
+    saveProduct( product: Product ) {
+      console.log('Guardando en base de datos', product );
+    };
+
+  };
+
+  class Mailer {
+
+    public masterEmail: string = 'email@email.com';
+   
+    sendEmail( emailList: string[], template: 'to-clients' | 'to-admins' ) {
+      console.log('Enviando correo a los clientes', template);
+    };
+  };
+
+  class ProductBloc {
+    
+    private productService: ProductService;
+    private mailer: Mailer;
+
+    constructor( productService: ProductService, mailer: Mailer ) {
+      this.productService = productService,
+      this.mailer = mailer;
+    };
+
+    loadProduct( id: number ) {
+      this.productService.getProduct( id );
+    };
+
+    saveProduct( product: Product ) {
+      this.productService.saveProduct( product );
+    };
+
+    notifyClients() {
+      this.mailer.sendEmail(['mycorreo@gmail.com'], 'to-clients' );
+    };
+  };
+  
+  class CartBloc {
+
+    private itemsInCart: Object[] = [];
+
+    addToCart( productId: number ) {
+      console.log('Agregando al carrito ', productId );
+    }
+  };
+
+  const productService = new ProductService();
+  const mailer = new Mailer();
+
+  const productBloc = new ProductBloc( productService, mailer );
+  const cartBlock = new CartBloc();
 
 })();
